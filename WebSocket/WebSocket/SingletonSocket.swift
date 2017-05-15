3e30deb71f37cf4c8b8b82b23a270b31b648d60d
Su Justin <202410089@s02.tku.edu.tw>
2017-05-15 20:41:30 +0800

    WebSocket first finish

---
+++ WebSocket/WebSocket/SingletonSocket.swift
@@ -0,0 +1,28 @@
+//
+//  Webscoket.swift
+//  WebSocket
+//
+//  Created by sujustin on 2017/5/14.
+//  Copyright © 2017年 sujustin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Starscream
+
+class SingletonSocket{
+    let socket: WebSocket = WebSocket(url: NSURL(string: "http://localhost:3000/")! as URL, protocols: ["room-chat"])
+    
+    class var sharedInstance : SingletonSocket{
+        struct Static{
+            static let instance: SingletonSocket = SingletonSocket()
+        }
+        
+        if !Static.instance.socket.isConnected{
+            Static.instance.socket.connect()
+        }
+        return Static.instance
+    }
+}
+
+//原文網址：https://read01.com/J0nagd.html
--- WebSocket/WebSocket/ViewController.swift
+++ WebSocket/WebSocket/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import Starscream
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WebSocketDelegate {
 
+    @IBOutlet weak var txetField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func connectAction(_ sender: UIButton) {
+        SingletonSocket.sharedInstance.socket.delegate = self
+        SingletonSocket.sharedInstance.socket.connect()
+        SingletonSocket.sharedInstance.socket.write(string: "hello world")
     }
-
-
+    
+    @IBAction func sendAction(_ sender: UIButton) {
+        SingletonSocket.sharedInstance.socket.write(string: txetField.text!) { 
+            print("***Message was send***")
+        }
+    }
+    
+    @IBAction func disConnectAction(_ sender: UIButton) {
+        SingletonSocket.sharedInstance.socket.disconnect()
+    }
+    
+    func websocketDidConnect(socket: WebSocket) {
+        print("Did Connect.......")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocket, data: Data) {
+        print("Did Receive Data: \(data.count)")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocket, error: NSError?) {
+        print("Did Disconnect!!!!!!!! [\(error)]")
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocket, text: String) {
+        
+        print("Receive Message: \(text)")
+    }
 }
-
